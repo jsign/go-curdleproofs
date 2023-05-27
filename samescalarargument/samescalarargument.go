d72a2ddadc0a3d2b94f86c04e63d3290893a0aa6
Ignacio Hagopian <jsign.uy@gmail.com>
2023-05-27 19:48:18 -0300

    samescalarargument: implement verifier
    Signed-off-by: Ignacio Hagopian <jsign.uy@gmail.com>

--- samescalarargument/samescalarargument.go
+++ samescalarargument/samescalarargument.go
@@ -24,7 +24,7 @@ func Prove(
 	R, S bls12381.G1Jac,
 	cm_T, cm_U groupcommitment.GroupCommitment,
 	k, r_t, r_u fr.Element,
-	transcript transcript.Transcript,
+	transcript *transcript.Transcript,
 	rand common.Rand,
 ) (Proof, error) {
 	r_a, err := rand.GetFr()
@@ -66,4 +66,33 @@ func Prove(
 		Z_t:  *z_t,
 		Z_u:  *z_u,
 	}, nil
+}
+
+func Verify(
+	proof *Proof,
+	crs CRS,
+	R, S bls12381.G1Jac,
+	cm_T, cm_U groupcommitment.GroupCommitment,
+	transcript *transcript.Transcript,
+) bool {
+	transcript.AppendPoints(
+		[]byte("sameexp_points"),
+		[]bls12381.G1Jac{
+			R, S,
+			cm_T.T_1, cm_T.T_2,
+			cm_U.T_1, cm_U.T_2,
+			proof.Cm_A.T_1, proof.Cm_A.T_2,
+			proof.Cm_B.T_1, proof.Cm_B.T_2,
+		},
+	)
+	alpha := transcript.GetChallenge([]byte("same_scalar_alpha"))
+
+	expected_1 := groupcommitment.New(&crs.G_t, &crs.H, (&bls12381.G1Jac{}).ScalarMultiplication(&R, common.FrToBigInt(&proof.Z_k)), &proof.Z_t)
+	expected_2 := groupcommitment.New(&crs.G_u, &crs.H, (&bls12381.G1Jac{}).ScalarMultiplication(&S, common.FrToBigInt(&proof.Z_k)), &proof.Z_u)
+
+	if proof.Cm_A.Add(cm_T.Mul(alpha)).Eq(expected_1) &&
+		proof.Cm_B.Add(cm_U.Mul(alpha)).Eq(expected_2) {
+		return true
+	}
+	return false
 }
