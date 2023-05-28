0899a719737a80637d2f2fd8ff4659f617944cd1
Ignacio Hagopian <jsign.uy@gmail.com>
2023-05-28 19:28:21 -0300

    common: add methods GetBlinders and SplitAt for IPA
    Signed-off-by: Ignacio Hagopian <jsign.uy@gmail.com>

--- common/util.go
+++ common/util.go
@@ -1,6 +1,7 @@
 package common
 
 import (
+	"fmt"
 	"math/big"
 
 	"github.com/consensys/gnark-crypto/ecc/bls12-381/fr"
@@ -10,4 +11,20 @@ func FrToBigInt(r *fr.Element) *big.Int {
 	var ret big.Int
 	r.BigInt(&ret)
 	return &ret
+}
+
+func GenerateBlindersFr(rand Rand, n uint) ([]fr.Element, error) {
+	var err error
+	ret := make([]fr.Element, n)
+	for i := uint(0); i < n; i++ {
+		ret[i], err = rand.GetFr()
+		if err != nil {
+			return nil, fmt.Errorf("generating Fr: %s", err)
+		}
+	}
+	return ret, nil
+}
+
+func SplitAt[T any](s []T, n uint) ([]T, []T) {
+	return s[:n], s[n:]
 }
