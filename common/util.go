b76e9beca250e42a5c1f4d555d2e21f788d7e9c1
Ignacio Hagopian <jsign.uy@gmail.com>
2023-05-30 19:43:56 -0300

    common: move gnark MSM config & remove blinders generator method since it is already part of rand
    Signed-off-by: Ignacio Hagopian <jsign.uy@gmail.com>

--- common/util.go
+++ common/util.go
@@ -1,28 +1,19 @@
 package common
 
 import (
-	"fmt"
 	"math/big"
+	"runtime"
 
+	"github.com/consensys/gnark-crypto/ecc"
 	"github.com/consensys/gnark-crypto/ecc/bls12-381/fr"
 )
+
+var MultiExpConf = ecc.MultiExpConfig{NbTasks: runtime.NumCPU()}
 
 func FrToBigInt(r *fr.Element) *big.Int {
 	var ret big.Int
 	r.BigInt(&ret)
 	return &ret
-}
-
-func GenerateBlindersFr(rand Rand, n uint) ([]fr.Element, error) {
-	var err error
-	ret := make([]fr.Element, n)
-	for i := uint(0); i < n; i++ {
-		ret[i], err = rand.GetFr()
-		if err != nil {
-			return nil, fmt.Errorf("generating Fr: %s", err)
-		}
-	}
-	return ret, nil
 }
 
 func SplitAt[T any](s []T, n uint) ([]T, []T) {
