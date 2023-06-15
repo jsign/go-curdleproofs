a9302a3ac865a3de723104ceab3c82c12782fd50
Ignacio Hagopian <jsign.uy@gmail.com>
2023-06-15 20:03:39 -0300

    util: add helper for tests
    Signed-off-by: Ignacio Hagopian <jsign.uy@gmail.com>

--- common/util.go
+++ common/util.go
@@ -1,10 +1,12 @@
 package common
 
 import (
+	"fmt"
 	"math/big"
 	"runtime"
 
 	"github.com/consensys/gnark-crypto/ecc"
+	bls12381 "github.com/consensys/gnark-crypto/ecc/bls12-381"
 	"github.com/consensys/gnark-crypto/ecc/bls12-381/fr"
 )
 
@@ -39,4 +41,51 @@ func Permute[T any](vs []T, perm []uint32) []T {
 		ret[i] = vs[perm[i]]
 	}
 	return ret
+}
+
+func ShufflePermuteCommit(
+	crsGs []bls12381.G1Affine,
+	crsHs []bls12381.G1Affine,
+	Rs []bls12381.G1Affine,
+	Ss []bls12381.G1Affine,
+	perm []uint32,
+	k fr.Element,
+	rand *Rand,
+) ([]bls12381.G1Affine, []bls12381.G1Affine, bls12381.G1Jac, []fr.Element, error) {
+
+	biK := FrToBigInt(&k)
+	Ts := make([]bls12381.G1Affine, len(Rs))
+	for i := range Ts {
+		Ts[i].ScalarMultiplication(&Rs[i], biK)
+	}
+
+	Us := make([]bls12381.G1Affine, len(Ss))
+	for i := range Us {
+		Us[i].ScalarMultiplication(&Ss[i], biK)
+	}
+
+	T := Permute(Ts, perm)
+	U := Permute(Us, perm)
+
+	rangeFrs := make([]fr.Element, len(crsGs))
+	for i := range perm {
+		rangeFrs[i] = fr.NewElement(uint64(perm[i]))
+	}
+	permRangeFrs := Permute(rangeFrs, perm)
+
+	rs_m, err := rand.GetFrs(N_BLINDERS)
+	if err != nil {
+		return nil, nil, bls12381.G1Jac{}, nil, fmt.Errorf("getting rs_m: %s", err)
+	}
+
+	var M1, M2 bls12381.G1Jac
+	if _, err := M1.MultiExp(crsGs, permRangeFrs, MultiExpConf); err != nil {
+		return nil, nil, bls12381.G1Jac{}, nil, fmt.Errorf("calculating M_1: %s", err)
+	}
+	if _, err := M2.MultiExp(crsHs, rs_m, MultiExpConf); err != nil {
+		return nil, nil, bls12381.G1Jac{}, nil, fmt.Errorf("calculating M_2: %s", err)
+	}
+	M1.AddAssign(&M2)
+
+	return T, U, M1, rs_m, nil
 }
