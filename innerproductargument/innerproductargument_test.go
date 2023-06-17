893b757e7cf84ee66d446a5b5dd06852a33ac221
Ignacio Hagopian <jsign.uy@gmail.com>
2023-06-17 13:58:25 -0300

    innerproductargument: avoid repetition in test
    Signed-off-by: Ignacio Hagopian <jsign.uy@gmail.com>

--- innerproductargument/innerproductargument_test.go
+++ innerproductargument/innerproductargument_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	bls12381 "github.com/consensys/gnark-crypto/ecc/bls12-381"
+	"github.com/consensys/gnark-crypto/ecc/bls12-381/fr"
 	"github.com/jsign/curdleproofs/common"
 	"github.com/jsign/curdleproofs/msmaccumulator"
 	"github.com/jsign/curdleproofs/transcript"
@@ -14,48 +15,13 @@ func TestInnerProductArgument(t *testing.T) {
 	t.Parallel()
 
 	n := 128
-	rand, err := common.NewRand(0)
-	require.NoError(t, err)
-
 	var proof Proof
 	{
-		transcriptProver := transcript.New([]byte("IPA"))
+		transcript := transcript.New([]byte("IPA"))
+		crs, B, C, z, bs, cs, _ := setup(t, n)
 
-		crsGs, err := rand.GetG1Affines(n)
+		rand, err := common.NewRand(42)
 		require.NoError(t, err)
-		// There is actually a relationship between crs_G_vec and crs_G_prime_vec because of the grandproduct optimization
-		// We generate a `vec_u` which has the discrete logs of every crs_G_prime element with respect to crs_G
-		us, err := rand.GetFrs(n)
-		require.NoError(t, err)
-		crsGs_prime := make([]bls12381.G1Affine, n)
-		for i := 0; i < n; i++ {
-			crsGs_prime[i].ScalarMultiplication(&crsGs[i], common.FrToBigInt(&us[i]))
-		}
-		H, err := rand.GetG1Jac()
-		require.NoError(t, err)
-		crs := CRS{
-			Gs:       crsGs,
-			Gs_prime: crsGs_prime,
-			H:        H,
-		}
-
-		// Generate some random vectors
-		bs, err := rand.GetFrs(n)
-		require.NoError(t, err)
-		cs, err := rand.GetFrs(n)
-		require.NoError(t, err)
-
-		z, err := common.IPA(bs, cs)
-		require.NoError(t, err)
-
-		// Create commitments
-		var B bls12381.G1Jac
-		_, err = B.MultiExp(crs.Gs, bs, common.MultiExpConf)
-		require.NoError(t, err)
-		var C bls12381.G1Jac
-		_, err = C.MultiExp(crs.Gs_prime, cs, common.MultiExpConf)
-		require.NoError(t, err)
-
 		proof, err = Prove(
 			crs,
 			B,
@@ -63,53 +29,18 @@ func TestInnerProductArgument(t *testing.T) {
 			z,
 			bs,
 			cs,
-			transcriptProver,
+			transcript,
 			rand,
 		)
 		require.NoError(t, err)
 	}
 
-	// Verify
-	{
-		rando, err := common.NewRand(0)
-		require.NoError(t, err)
-
-		transcriptVerifier := transcript.New([]byte("IPA"))
+	t.Run("Completeness", func(t *testing.T) {
+		transcript := transcript.New([]byte("IPA"))
 		msmAccumulator := msmaccumulator.New()
+		crs, B, C, z, _, _, us := setup(t, n)
 
-		crsGs, err := rando.GetG1Affines(n)
-		require.NoError(t, err)
-		// There is actually a relationship between crs_G_vec and crs_G_prime_vec because of the grandproduct optimization
-		// We generate a `vec_u` which has the discrete logs of every crs_G_prime element with respect to crs_G
-		us, err := rando.GetFrs(n)
-		require.NoError(t, err)
-		crsGs_prime := make([]bls12381.G1Affine, n)
-		for i := 0; i < n; i++ {
-			crsGs_prime[i].ScalarMultiplication(&crsGs[i], common.FrToBigInt(&us[i]))
-		}
-		H, err := rando.GetG1Jac()
-		require.NoError(t, err)
-		crs := CRS{
-			Gs:       crsGs,
-			Gs_prime: crsGs_prime,
-			H:        H,
-		}
-
-		// Generate some random vectors
-		bs, err := rando.GetFrs(n)
-		require.NoError(t, err)
-		cs, err := rando.GetFrs(n)
-		require.NoError(t, err)
-
-		z, err := common.IPA(bs, cs)
-		require.NoError(t, err)
-
-		// Create commitments
-		var B bls12381.G1Jac
-		_, err = B.MultiExp(crs.Gs, bs, common.MultiExpConf)
-		require.NoError(t, err)
-		var C bls12381.G1Jac
-		_, err = C.MultiExp(crs.Gs_prime, cs, common.MultiExpConf)
+		rand, err := common.NewRand(43)
 		require.NoError(t, err)
 
 		ok, err := Verify(
@@ -119,7 +50,7 @@ func TestInnerProductArgument(t *testing.T) {
 			C,
 			z,
 			us,
-			transcriptVerifier,
+			transcript,
 			msmAccumulator,
 			rand,
 		)
@@ -129,5 +60,47 @@ func TestInnerProductArgument(t *testing.T) {
 		ok, err = msmAccumulator.Verify()
 		require.NoError(t, err)
 		require.True(t, ok)
+	})
+}
+
+func setup(t *testing.T, n int) (CRS, bls12381.G1Jac, bls12381.G1Jac, fr.Element, []fr.Element, []fr.Element, []fr.Element) {
+	rand, err := common.NewRand(0)
+	require.NoError(t, err)
+
+	crsGs, err := rand.GetG1Affines(n)
+	require.NoError(t, err)
+	// There is actually a relationship between crs_G_vec and crs_G_prime_vec because of the grandproduct optimization
+	// We generate a `vec_u` which has the discrete logs of every crs_G_prime element with respect to crs_G
+	us, err := rand.GetFrs(n)
+	require.NoError(t, err)
+	crsGs_prime := make([]bls12381.G1Affine, n)
+	for i := 0; i < n; i++ {
+		crsGs_prime[i].ScalarMultiplication(&crsGs[i], common.FrToBigInt(&us[i]))
 	}
+	H, err := rand.GetG1Jac()
+	require.NoError(t, err)
+	crs := CRS{
+		Gs:       crsGs,
+		Gs_prime: crsGs_prime,
+		H:        H,
+	}
+
+	// Generate some random vectors
+	bs, err := rand.GetFrs(n)
+	require.NoError(t, err)
+	cs, err := rand.GetFrs(n)
+	require.NoError(t, err)
+
+	z, err := common.IPA(bs, cs)
+	require.NoError(t, err)
+
+	// Create commitments
+	var B bls12381.G1Jac
+	_, err = B.MultiExp(crs.Gs, bs, common.MultiExpConf)
+	require.NoError(t, err)
+	var C bls12381.G1Jac
+	_, err = C.MultiExp(crs.Gs_prime, cs, common.MultiExpConf)
+	require.NoError(t, err)
+
+	return crs, B, C, z, bs, cs, us
 }
