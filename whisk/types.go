b24c89ea470e42d0f95ccf2c45eeabfd28be00fc
Ignacio Hagopian <jsign.uy@gmail.com>
2023-07-03 19:56:26 -0300

    whisk: improvements and fixes in types
    Signed-off-by: Ignacio Hagopian <jsign.uy@gmail.com>

--- whisk/types.go
+++ whisk/types.go
@@ -12,16 +12,18 @@ import (
 )
 
 const (
-	G1POINT_SIZE                 = 48
-	WHISK_MAX_SHUFFLE_PROOF_SIZE = 1 << 15
-	WHISK_MAX_OPENING_PROOF_SIZE = 1 << 10
-	TRACKER_PROOF_SIZE           = 128
-	N                            = 128
-	ELL                          = N - common.N_BLINDERS
+	G1POINT_SIZE = 48
+
+	N   = 128
+	ELL = N - common.N_BLINDERS
+
+	TRACKER_PROOF_SIZE       = 128
+	WHISK_SHUFFLE_PROOF_SIZE = 4576
 )
 
 type G1PointBytes [G1POINT_SIZE]byte
 type TrackerProofBytes [TRACKER_PROOF_SIZE]byte
+type WhiskShuffleProofBytes [WHISK_SHUFFLE_PROOF_SIZE]byte
 
 var g1Gen bls12381.G1Affine
 
@@ -37,27 +39,36 @@ type WhiskShuffleProof struct {
 func (wsp *WhiskShuffleProof) FromReader(r io.Reader) error {
 	// TODO(jsign): revisit since "decoder" for single element is overkill
 	d := bls12381.NewDecoder(r)
-	if err := d.Decode(&wsp.M); err != nil {
+	var tmp bls12381.G1Affine
+	if err := d.Decode(&tmp); err != nil {
 		return fmt.Errorf("failed to decode M: %v", err)
 	}
+	wsp.M.FromAffine(&tmp)
 	if err := wsp.Proof.FromReader(r); err != nil {
 		return fmt.Errorf("failed to decode proof: %v", err)
 	}
 	return nil
 }
 
-func (wsp *WhiskShuffleProof) Serialize() ([]byte, error) {
-	buf := bytes.NewBuffer(make([]byte, WHISK_MAX_SHUFFLE_PROOF_SIZE))
+func (wsp *WhiskShuffleProof) Serialize() (WhiskShuffleProofBytes, error) {
+	buf := bytes.NewBuffer(make([]byte, 0, WHISK_SHUFFLE_PROOF_SIZE))
 	e := bls12381.NewEncoder(buf)
 
-	if err := e.Encode(wsp.M); err != nil {
-		return nil, fmt.Errorf("failed to encode M: %v", err)
-	}
-	if err := wsp.Proof.Serialize(buf); err != nil {
-		return nil, fmt.Errorf("failed to encode proof: %v", err)
+	var tmp bls12381.G1Affine
+	tmp.FromJacobian(&wsp.M)
+	if err := e.Encode(&tmp); err != nil {
+		return WhiskShuffleProofBytes{}, fmt.Errorf("failed to encode M: %v", err)
 	}
 
-	return buf.Bytes(), nil
+	if err := wsp.Proof.Serialize(buf); err != nil {
+		return WhiskShuffleProofBytes{}, fmt.Errorf("failed to encode proof: %v", err)
+	}
+
+	proofBytes := buf.Bytes()
+	var ret WhiskShuffleProofBytes
+	copy(ret[:], proofBytes)
+
+	return ret, nil
 }
 
 type WhiskTracker struct {
@@ -73,8 +84,7 @@ func NewWhiskTracker(rG, krG bls12381.G1Affine) WhiskTracker {
 }
 
 func (wt *WhiskTracker) getPoints() (bls12381.G1Affine, bls12381.G1Affine, error) {
-	var rG bls12381.G1Affine
-	var krG bls12381.G1Affine
+	var rG, krG bls12381.G1Affine
 	if _, err := rG.SetBytes(wt.rG[:]); err != nil {
 		return bls12381.G1Affine{}, bls12381.G1Affine{}, fmt.Errorf("failed to set rG: %v", err)
 	}
@@ -107,9 +117,8 @@ func (tp *TrackerProof) FromBytes(buf TrackerProofBytes) error {
 }
 
 func (tp *TrackerProof) Serialize() TrackerProofBytes {
-	// TODO(jsign): avoid alloc.
-	var buf bytes.Buffer
-	e := bls12381.NewEncoder(&buf)
+	buf := bytes.NewBuffer(make([]byte, 0, TRACKER_PROOF_SIZE))
+	e := bls12381.NewEncoder(buf)
 	// bytes.Buffer, by design, never fails as an io.Writer.
 	_ = e.Encode(&tp.A)
 	_ = e.Encode(&tp.B)
