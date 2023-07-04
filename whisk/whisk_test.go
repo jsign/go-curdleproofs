8cf3c2f8171063c8280e7892cae716b5e60f0b25
Ignacio Hagopian <jsign.uy@gmail.com>
2023-07-03 21:28:01 -0300

    whisk: add whisk shuffle proof e2e test
    Signed-off-by: Ignacio Hagopian <jsign.uy@gmail.com>

--- whisk/whisk_test.go
+++ whisk/whisk_test.go
@@ -5,6 +5,7 @@ import (
 
 	bls12381 "github.com/consensys/gnark-crypto/ecc/bls12-381"
 	"github.com/consensys/gnark-crypto/ecc/bls12-381/fr"
+	curdleproof "github.com/jsign/curdleproofs"
 	"github.com/jsign/curdleproofs/common"
 	"github.com/stretchr/testify/require"
 )
@@ -27,10 +28,31 @@ func TestWhiskTrackerProof(t *testing.T) {
 	require.True(t, ok)
 
 	// Assert correct TRACKER_PROOF_SIZE
-	var trackerProof2 TrackerProof
-	err = trackerProof2.FromBytes(trackerProof)
+	// Note: this part of the reference test isn't implemented since
+	//       in this implementation the serialized proof is forced to
+	//       be an array of length equal TRACKER_PROOF_SIZE.
+}
+
+func TestWhiskShuffleProof(t *testing.T) {
+	rand, err := common.NewRand(0)
 	require.NoError(t, err)
-	require.Equal(t, TRACKER_PROOF_SIZE, len(trackerProof2.Serialize()))
+
+	crs, err := curdleproof.GenerateCRS(ELL, rand)
+	require.NoError(t, err)
+
+	shuffledTrackers := generateShuffleTrackers(t, rand)
+
+	postTrackers, proofBytes, err := GenerateWhiskShuffleProof(crs, shuffledTrackers, rand)
+	require.NoError(t, err)
+
+	ok, err := IsValidWhiskShuffleProof(crs, shuffledTrackers, postTrackers, proofBytes, rand)
+	require.NoError(t, err)
+	require.True(t, ok)
+
+	// Assert correct WHISK_SHUFFLE_PROOF_SIZE
+	// Note: this part of the reference test isn't implemented since
+	//       in this implementation the serialized proof is forced to
+	//       be an array of length equal WHISK_SHUFFLE_PROOF_SIZE.
 }
 
 func generateTracker(t *testing.T, rand *common.Rand, k fr.Element) WhiskTracker {
@@ -50,4 +72,14 @@ func computeTracker(k, r fr.Element) WhiskTracker {
 func getKComm(k fr.Element) G1PointBytes {
 	var res bls12381.G1Affine
 	return res.ScalarMultiplication(&g1Gen, common.FrToBigInt(&k)).Bytes()
+}
+
+func generateShuffleTrackers(t *testing.T, rand *common.Rand) []WhiskTracker {
+	wts := make([]WhiskTracker, ELL)
+	for i := 0; i < ELL; i++ {
+		k, err := rand.GetFr()
+		require.NoError(t, err)
+		wts[i] = generateTracker(t, rand, k)
+	}
+	return wts
 }
